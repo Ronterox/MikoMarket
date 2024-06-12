55b5b4c6f840198e3756f05f841470c221a383f3
Ricardo <ricarditomontserrat@gmail.com>
2024-06-12 22:40:49 +0200

    added foreach to custom parser

--- view.php
+++ view.php
@@ -9,20 +9,18 @@ function view(string $filename, array $data = [], string $block = null)
         '/{{\s*(.+?)\s*}}/' => '<?= $1 ?>', // {{ $var }}
         '/@require\s+(.+)/' => '<?php require $1; ?>', // @require 'file'
         '/@block\s+.+?@endblock/s' => '', // @block ... @endblock
+        '/@foreach\s+(.+?)\s+in\s+(.+?)[\r\n]+(.+?)@endforeach/s' => '<?php foreach($2 as $1): extract($1); ?>$3<?php endforeach; ?>', // @foreach $var in $array ... @endforeach
     ];
 
     $html = file_get_contents($file);
-
     $html = preg_replace_callback('/@render\s+(.+)/', function ($renders) use ($html) {
         preg_match("/@block\s+{$renders[1]}\s*(.+?)@endblock/s", $html, $block);
         return $block[1];
     }, $html);
-
     if ($block) {
         preg_match("/@block\s+{$block}\s*(.+?)@endblock/s", $html, $block);
         $html = $block[1];
     }
-
     $html = preg_replace(array_keys($patterns), array_values($patterns), $html);
 
 
