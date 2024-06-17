c7cad2e1276f3bf4021c7b7969f7c4e7168f6191
Ricardo <ricarditomontserrat@gmail.com>
2024-06-17 13:32:58 +0200

    Simulation form instead of button, and sends order data for winning calculation

--- routes/candles.php
+++ routes/candles.php
@@ -17,6 +17,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         } else {
             $max_value = 15;
             $num_candles = filter_input(INPUT_GET, 'num_candles', FILTER_VALIDATE_INT) ?? 10;
+            $order = filter_input(INPUT_GET, 'order', FILTER_VALIDATE_INT);
 
             $rng = fn () => rand(-$max_value, $max_value - 1) + 1;
             $rnghigh = fn () => rand($max_value - 2, $max_value);
@@ -32,16 +33,17 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 return compact('high', 'low', 'open', 'close');
             }, $highs, $lows, $opens, $closes);
 
-            view('candles', compact('candles', 'closes'));
+            $winnings = $order ? end($closes) - $order : 0;
+            view('candles', compact('candles', 'closes', 'winnings'));
         }
         break;
     case 'POST':
         $speed = filter_input(INPUT_POST, 'speed', FILTER_VALIDATE_FLOAT);
         $simulate = filter_input(INPUT_POST, 'simulate', FILTER_VALIDATE_BOOLEAN);
         if ($simulate) {
-            view('index', compact('speed'), 'simulating-button');
+            view('index', compact('speed'), 'simulating-form');
         } else {
-            view('index', [], 'simulation-button');
+            view('index', [], 'simulation-form');
         }
         break;
     default:
